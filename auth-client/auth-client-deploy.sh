728650a1c60b29949c1b2b746bbf15e9b0113b2b
american-worx <khan@americanworx.com>
2024-11-13 09:31:00 -0500

    kube

--- auth-client/auth-client-deploy.sh
+++ auth-client/auth-client-deploy.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 
-kubectl apply  -n default -f auth-client-claim1-persistentvolumeclaim.yaml,auth-client-deployment.yaml,auth-client-service.yaml,mysql-data-persistentvolumeclaim.yaml,mysqldb-client-service.yaml,auth-client-cm0-configmap.yaml,auth-client-deploy.sh,mysqldb-client-deployment.yaml
+kubectl apply  -n default -f auth-client-claim1-persistentvolumeclaim.yaml,auth-client-deployment.yaml,auth-client-service.yaml,mysql-data-persistentvolumeclaim.yaml,mysqldb-service.yaml,auth-client-cm0-configmap.yaml,auth-client-deploy.sh,mysqldb-deployment.yaml
--- auth/auth-deploy.sh
+++ auth/auth-deploy.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 
-kubectl apply  -n default -f auth-claim1-persistentvolumeclaim.yaml,auth-deployment.yaml,auth-service.yaml,mysql-data-persistentvolumeclaim.yaml,mysqldb-auth-service.yaml,auth-cm0-configmap.yaml,auth-deploy.sh,mysqldb-auth-deployment.yaml,pv-volume.yaml  
+kubectl apply  -n default -f auth-claim1-persistentvolumeclaim.yaml,auth-deployment.yaml,auth-service.yaml,mysql-data-persistentvolumeclaim.yaml,mysqldb-service.yaml,auth-cm0-configmap.yaml,auth-deploy.sh,mysqldb-deployment.yaml,pv-volume.yaml  
